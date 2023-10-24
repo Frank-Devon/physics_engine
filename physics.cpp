bd519414ea0dace894b4265dad66f6c68229cd87
Daniel <98561131+Frank-Devon@users.noreply.github.com>
2023-10-23 17:18:27 -0700

    Update physics.cpp

--- physics.cpp
+++ physics.cpp
@@ -25,6 +25,7 @@ void Ball::integrate(var_type duration)
     force_accumulator = Vector2(0.0, 0.0);
 }
 
+// checks if balls have collided then fixes position and velocity. 
 void Ball::collides_ball(Ball& b) 
 {
     Vector2 pos_relative = b.pos - pos;
@@ -41,10 +42,6 @@ void Ball::collides_ball(Ball& b)
         var_type distance_squared = pow(pos_relative.magnitude(), 2.0);
         new_vel = vel - (2.0 * b_mass / mass_sum) * (dot_0 / distance_squared) * (pos - b.pos); 
         b_new_vel = b.vel - (2.0 * mass / mass_sum) * (dot_1 / distance_squared) * (b.pos - pos); 
-        //new_vel = (mass * vel + b_mass * b.vel - b_mass * restitution * vel
-        //    + b_mass * restitution * b.vel) / ( mass + b_mass);
-        //b_new_vel = (mass * vel + b_mass * b.vel - mass * restitution * b.vel
-        //    + mass * restitution * vel) / ( mass + b_mass);
         // assign new velocities
         vel = new_vel;
         b.vel = b_new_vel;
@@ -60,6 +57,7 @@ void Ball::collides_ball(Ball& b)
     }
 }
 
+// checks if ball has collided with edge then fixes position and velocity
 void Ball::collides_edge(const Edge& edge) 
 {
     // P S E are 2d Vectors (Vec2)
@@ -107,8 +105,7 @@ Edge::Edge(Vector2 start, Vector2 end) : start(start), end(end)
     normal = tangent.perpendicular();
 }
 
-    Vector2 
-Collision::reflect(const Vector2& a, const Edge& edge)
+Vector2 Collision::reflect(const Vector2& a, const Edge& edge)
 {
     var_type v_dot_t = a.dot(edge.tangent); //(Vec2_dot_product(a, &edge->tangent));
     var_type v_dot_n = a.dot(edge.normal);//(Vec2_dot_product(a, &edge->normal));
@@ -116,18 +113,15 @@ Collision::reflect(const Vector2& a, const Edge& edge)
     Vector2 tangent = edge.tangent;  // might be adjusted
     if (v_dot_n > 0) {
         v_dot_n = -1.0 * v_dot_n;
-        normal = - edge.normal; //Vec2_scale(&normal, -1.0);
+        normal = - edge.normal; 
     }
     if (v_dot_t < 0) {
         v_dot_t = -1.0 * v_dot_t;
-        tangent = -1.0 * tangent; //Vec2_scale(&tangent, -1.0);
+        tangent = -1.0 * tangent; 
     }
-    //printf("--- v_dot_t and n: %f, %f\n", v_dot_t, v_dot_n);
-    Vector2 v_dot_t_vec = v_dot_t * tangent;  //Vec2_scale(&tangent, v_dot_t);
-    Vector2 v_dot_n_vec = v_dot_n * normal ;  //Vec2_scale(&normal, v_dot_n);
-    //printf("--- v_dot_t_vec and n: %f, %f\n", v_dot_t_vec, v_dot_n_vec);
-    Vector2 result = v_dot_t_vec - v_dot_n_vec; //Vec2_sub(&v_dot_t_vec, &v_dot_n_vec);
-    //printf("--- result: %f, %f\n", result.x, result.y);
+    Vector2 v_dot_t_vec = v_dot_t * tangent; 
+    Vector2 v_dot_n_vec = v_dot_n * normal ;  
+    Vector2 result = v_dot_t_vec - v_dot_n_vec; 
     return result;
 }
 
